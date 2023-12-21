3477fd4d2e76e04addb2c3ac45ba41cef62dbf28
danielpengpartner <Daniel.Peng@partner.bmwgroup.com>
2023-12-21 17:46:01 +0800

    [23/12/21] 增加了字符串变换

--- main.cpp
+++ main.cpp
@@ -28,6 +28,55 @@ std::optional<std::string> getTestString()
     }
 }
 
+// 分隔符定义为constexpr字符数组
+constexpr char DelimiterStart = '#';
+constexpr char DelimiterMiddle = '|';
+
+std::optional<std::pair<std::string, std::string>> extractIdAndMessage(const std::string& command)
+{
+    const auto firstDelim = command.find(DelimiterMiddle);
+    const auto lastDelim = command.rfind(DelimiterMiddle);
+
+    if (firstDelim == std::string::npos || firstDelim == lastDelim)
+    {
+        return std::nullopt;
+    }
+
+    std::string id = command.substr(firstDelim + 1, lastDelim - firstDelim - 1);
+    std::string message = command.substr(lastDelim + 1);
+    cout << "id = " << id << ", MSG = " << message << endl;
+
+    return std::make_pair(id, message);
+}
+
+std::optional<std::string> replaceCommandWithString(std::string& messageText) {
+    const auto startPos = messageText.find(DelimiterStart);
+    const auto endPos = messageText.rfind(DelimiterStart);
+
+    if (startPos == std::string::npos || endPos == std::string::npos || startPos == endPos)
+    {
+        return std::nullopt;
+    }
+
+    auto extracted = extractIdAndMessage(messageText.substr(startPos + 1, endPos - startPos - 1));
+
+    if (!extracted.has_value())
+    {
+        return std::nullopt;
+    }
+
+    if (0)
+    {
+        messageText = messageText.substr(0, startPos);
+    }
+    else
+    {
+        messageText.replace(startPos, endPos - startPos + 1, extracted->second);
+    }
+
+    return extracted->second;
+}
+
 int main()
 {
     const auto testString = getTestString();
@@ -49,4 +98,19 @@ int main()
     cam.process_event(EvShutterFull("enter shooting"));
     cam.process_event(EvShutterRelease("enter NoShooting"));
     cam.process_event(EvConfig("enter config"));
+
+    // Test String
+    cout << "\n###################################" << endl;
+    std::string inputStr = "in 400 meters, enter the tunnel#SLOW_DOWN|1|, please slow down#";
+
+    auto message = replaceCommandWithString(inputStr);
+
+    if (!message)
+    {
+        std::cerr << "Failed to replace command in the string." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Message: " << *message << std::endl;
+    std::cout << "Replaced String: " << inputStr << std::endl;
 }
