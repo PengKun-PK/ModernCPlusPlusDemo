71e85857fed2438e6ada2f8d343e43c6a97a89ce
danielpengpartner <Daniel.Peng@partner.bmwgroup.com>
2024-01-10 17:46:34 +0800

    [24/01/10] 新增c++20的source_location调用示例，用于log输出等的打印

--- main.cpp
+++ main.cpp
@@ -15,6 +15,21 @@ using namespace MathFunctions;
 using namespace StateMachine;
 
 const std::string testString = "TEST Console Start!";
+constexpr double BASE_SPEED_1 = 111.0945;
+constexpr double MAX_SPEED_1 = 30;
+constexpr double SCALE_DISTANCE_1 = 392.9842;
+constexpr double EXPONENT_1 = 1.972242;
+
+constexpr double BASE_SPEED_2 = 90.03563;
+constexpr double MAX_SPEED_2 = 60;
+constexpr double SCALE_DISTANCE_2 = 314.6796;
+constexpr double EXPONENT_2 = 14.54655;
+
+
+constexpr double BASE_SPEED_3 = 60;
+constexpr double MAX_SPEED_3 = 20;
+constexpr double SCALE_DISTANCE_3 = 100;
+constexpr double EXPONENT_3 = 1.584963;
 
 std::optional<std::string> getTestString()
 {
@@ -42,9 +57,14 @@ std::optional<std::pair<std::string, std::string>> extractIdAndMessage(const std
         return std::nullopt;
     }
 
+    std::string strType = command.substr(0, firstDelim);
     std::string id = command.substr(firstDelim + 1, lastDelim - firstDelim - 1);
     std::string message = command.substr(lastDelim + 1);
-    cout << "id = " << id << ", MSG = " << message << endl;
+    cout << "strType = " << strType << ", id = " << id << ", MSG = " << message << endl;
+    if (command.find("SLOW_DOWN") != std::string::npos)
+    {
+        cout << "find SLOW_DOWN." << endl;
+    }
 
     return std::make_pair(id, message);
 }
@@ -101,7 +121,7 @@ int main()
 
     // Test String
     cout << "\n###################################" << endl;
-    std::string inputStr = "in 400 meters, enter the tunnel#SLOW_DOWN|1|, please slow down#";
+    std::string inputStr = "in 400 meters, enter the tunnel#SLOW_DWON|2|, please slow down#";
 
     auto message = replaceCommandWithString(inputStr);
 
@@ -111,6 +131,23 @@ int main()
         return -1;
     }
 
+    // Log
+    const auto location = source_location::current();
+    std::cout << "This file : " << location.file_name()
+              << ", line : " << location.line()
+              << ", function : " << location.function_name()
+              << ", column : " << location.column()
+              << '\n';
     std::cout << "Message: " << *message << std::endl;
     std::cout << "Replaced String: " << inputStr << std::endl;
+
+    // Test Algorithm
+    cout << "\n###################################" << endl;
+    auto safeSpeed1 = BASE_SPEED_1 + (MAX_SPEED_1 - BASE_SPEED_1) / (1 + pow((400.0 / SCALE_DISTANCE_1), EXPONENT_1));
+
+    auto safeSpeed2 = BASE_SPEED_2 + (MAX_SPEED_2 - BASE_SPEED_2) / (1 + pow((400.0 / SCALE_DISTANCE_2), EXPONENT_2));
+
+    auto safeSpeed3 = BASE_SPEED_3 + (MAX_SPEED_3 - BASE_SPEED_3) / (1 + pow((400.0 / SCALE_DISTANCE_3), EXPONENT_3));
+
+    cout << "safeSpeed1 = " << safeSpeed1 << ", safeSpeed2 = " << safeSpeed2 << ", safeSpeed3 = " << safeSpeed3;
 }
