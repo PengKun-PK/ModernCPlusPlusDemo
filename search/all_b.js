376f3673a18628098e063dcaf9b2a0e870d2b69e
PengKun-PK <PengKun-PK@users.noreply.github.com>
2026-03-17 02:55:23 +0000

    docs: update documentation 8f8706e50aa389f4ebc1702f5c221b146529cbd1

---
+++ search/all_b.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['nextobserverid_5f_0',['nextObserverId_',['../classSubject.html#a7a0f06ec50a624094dc95c81c83d39fd',1,'Subject']]],
+  ['nextsubscriptionid_5f_1',['nextSubscriptionId_',['../classEventBus.html#a56e97c8df228288ffb60d3cdbf332e3e',1,'EventBus']]],
+  ['no_5ftransition_2',['no_transition',['../classStateMachine__.html#a6f90011bbb1f0db4cfe3315471b7635b',1,'StateMachine_']]],
+  ['notify_3',['notify',['../classSubject.html#a73c5f190e55850a9fb198ea6faec937f',1,'Subject::notify()'],['../classcomm_1_1AttributeSync.html#a19194ebad3c20ca05a049189c22de0d7',1,'comm::AttributeSync::notify()'],['../classcomm_1_1Attribute.html#af2610ea0b3a8354fac8555665dc215f6',1,'comm::Attribute::notify()'],['../classcomm_1_1EventSync.html#aab125ac84f8ef2718a98d2087a527a23',1,'comm::EventSync::notify()'],['../classcomm_1_1Event.html#acf3b78b7e915038173174a7e7f19db4e',1,'comm::Event::notify()']]],
+  ['notifyasync_4',['notifyAsync',['../classcomm_1_1Subscribable.html#a6532f4ab2baf2f36cb6d26fe02b893ae',1,'comm::Subscribable']]],
+  ['notifysync_5',['notifySync',['../classcomm_1_1Subscribable.html#a34040cfc21bd57e0621abbca8391f9f5',1,'comm::Subscribable']]],
+  ['notshooting_6',['NotShooting',['../structStateMachine_1_1NotShooting.html',1,'StateMachine']]]
+];
---
+++ search/variables_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['queue_5fmutex_0',['queue_mutex',['../classThreadPool.html#adf76c928f148e8fdcd7e34c2bbaa97e3',1,'ThreadPool']]]
+];
